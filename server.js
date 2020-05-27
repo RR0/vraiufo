dedad7cb419079396b6dc60c567bdac70d728c83
Jérôme Beau <javarome@gmail.com>
2020-05-27 16:07:52 +0200

    Added id parameter

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ fastify.register(
     fastifyCaching,
     {privacy: fastifyCaching.privacy.NOCACHE},
     (err) => {
-        if (err) throw err
+      if (err) throw err
     }
 )
 const path = require('path')
@@ -21,20 +21,37 @@ const pool = new Pool({
     database: 'vraiufodb',
     user: 'javarome'
 })
-
-// Declare a route
+const uuidPattern = '[0-9a-fA-F]{8}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{12}';
 fastify.get('/', async (req, res) => {
     const accept = req.accepts()
     const lang = accept.language(['fr', 'en'])
     const client = await pool.connect()
     try {
-        const query = `SELECT * FROM ${lang}.ufo`
+        const query = `SELECT id FROM ${lang}.ufo`
         console.log(query)
         const result = await client.query(query)
         const size = result.rows.length;
         const n = Math.floor(Math.random() * size)
         console.log(`picking row #${n} among ${size}`)
         const row = result.rows[n]
+        res.redirect(`/${row.id}`)
+    } finally {
+        client.release()
+    }
+})
+fastify.get(`/:id(${uuidPattern})`, async (req, res) => {
+    const accept = req.accepts()
+    const lang = accept.language(['fr', 'en'])
+    const client = await pool.connect()
+    try {
+        const query = {
+            name: 'fetch-ufo',
+            text: `SELECT * FROM ${lang}.ufo WHERE id=$1`,
+            values: [req.params.id],
+        }
+        console.log(query)
+        const result = await client.query(query)
+        const row = result.rows[0]
         let rendered = await Sqrl.renderFile(`index_${lang}.html`, row)
         res
             .code(200)
